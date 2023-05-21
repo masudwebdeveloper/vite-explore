f84787deec01931dfc1652876fe6c4d0be544b1f
masudrana5 <abdullainfo30@gmail.com>
2023-05-21 10:29:17 +0600

    vite setup with tailwind done

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,20 @@
+const Home = () => {
+  return <>
+  <main className="grid gap-4 grid-cols-12 grid-rows-3 h-[100vh] bg-white">
+    <div className="bg-black text-white flex items-center justify-center h-full col-span-4">Hello 1</div>
+    <div className="bg-black text-white flex items-center justify-center h-full col-span-2">Hello 2</div>
+    <div className="bg-black text-white flex items-center justify-center h-full col-span-4">Hello 3</div>
+    <div className="bg-black text-white flex items-center justify-center h-full col-span-2">Hello 4</div>
+    <div className="bg-black text-white flex items-center justify-center h-full col-span-2">Hello 5</div>
+    <div className="bg-black text-white flex items-center justify-center h-full row-span-2 col-span-4">Hello 6</div>
+    <div className="bg-black text-white flex items-center justify-center h-full col-span-2">Hello 7</div>
+    <div className="bg-black text-white flex items-center justify-center h-full col-span-2">Hello 8</div>
+    <div className="bg-black text-white flex items-center justify-center h-full col-span-2">Hello 9</div>
+    <div className="bg-black text-white flex items-center justify-center h-full col-span-2">Hello 10</div>
+    <div className="bg-black text-white flex items-center justify-center h-auto col-span-2 row-auto">Hello 11</div>
+    <div className="bg-black text-white flex items-center justify-center h-full col-span-4">Hello 12</div>
+  </main>
+  </>;
+};
+
+export default Home;
